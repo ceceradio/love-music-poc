b6c9e99c59958e7855cb9a2d612eb8ea478f5913
Cecelia Wren <cecelia.wren@6dglobal.com>
2017-10-09 19:30:36 -0400

    press a to do the thing

--- main.lua
+++ main.lua
@@ -1,14 +1,47 @@
 function love.load()
-   soundData = love.sound.newSoundData("hat.wav")
-   hatSource = love.audio.newSource(soundData)
+   hatSource = love.audio.newSource(love.sound.newSoundData("hat.wav"))
+   --hatSource:setLooping(true)
+   hatSource:setVolume(0.5)
+   tickDuration = 1 / 240
    love.audio.play(hatSource)
+   boopSource = love.audio.newSource(love.sound.newSoundData("g1s-2.wav"))
 end
-
+-- 3857
 function love.update(dt)
-   -- tick math
+-- inbetween tick math
+    if (boopShouldPlay and not boopSource:isStopped()) then
+        tellVal = boopSource:tell("samples")
+        if (tellVal > 18138) then
+            --[[while (tellVal > 18137 + 17074) do
+                tellVal = tellVal - (18137 - 17074);
+            end]]
+            boopSource:seek(17073 + (tellVal - 18138), "samples")
+        end
+    end
+    -- tick math
+    if (hatSource:isStopped()) then
+        if (boopShouldPlay and boopSource:isStopped()) then
+            love.audio.play(boopSource)
+        end
+        love.audio.play(hatSource)
+    end
+    love.timer.sleep(tickDuration - dt);
 end
 
 function love.draw()
     love.graphics.print("Hello World", 400, 300)
 end
 
+function love.keypressed(key, scancode, isrepeat)
+    if (key == "a") then
+        boopSource:stop();
+        boopSource:seek(0, "samples");
+        boopShouldPlay = true
+    end 
+end
+
+function love.keyreleased(key, scancode)
+    if (key == "a") then
+        boopShouldPlay = false
+    end 
+end
